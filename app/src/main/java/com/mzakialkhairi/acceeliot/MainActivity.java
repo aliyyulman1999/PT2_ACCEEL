bb6046cab58587c10c34f5bd7a133ecc6e7affeb
Muhammad Zaki Alkhairi <muhammadzakialkhairi@gmail.com>
2020-01-31 01:32:41 +0700

    First Commit.

---
+++ app/src/main/java/com/mzakialkhairi/acceeliot/MainActivity.java
@@ -0,0 +1,24 @@
+// code by Muhammad Zaki Alkhairi
+
+package com.mzakialkhairi.acceeliot;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Mode(View view){
+        Intent intent = new Intent(MainActivity.this, ModeActivity.class);
+        startActivity(intent);
+    }
+
+}
+
